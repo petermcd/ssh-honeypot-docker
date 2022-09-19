b9c5e5f2d1bc86327f0fafd19cf025da82ed78a4
Peter McDonald <me@petermcdonald.co.uk>
2022-09-19 23:22:41 +0100

    Implementing minimal build

---
+++ make.sh
@@ -0,0 +1,12 @@
+VERSION="V_9_0_P1"
+
+docker build --tag full -f ssh_honeypot.dockerfile .;
+IMAGE_ID=$(docker run -d full);
+
+python3 -m venv venv;
+./venv/bin/pip install git+https://github.com/petermcd/docker-build.git;
+./venv/bin/python build_dockerfile.py "$IMAGE_ID";
+
+docker stop "$IMAGE_ID";
+docker build --tag petermcd/ssh_honeypot:$VERSION .;
+docker tag petermcd/ssh_honeypot:$VERSION petermcd/ssh_honeypot:latest;
