43150721c1cf13d1813431070b8c650810b91e03
Peter McDonald <me@petermcdonald.co.uk>
2022-09-20 01:02:48 +0100

    corrected make.sh

--- make.sh
+++ make.sh
@@ -4,9 +4,11 @@ docker build --tag full -f ssh_honeypot.dockerfile .;
 IMAGE_ID=$(docker run -d full);
 
 python3 -m venv venv;
+./venv/bin/python -m pip install --update pip;
 ./venv/bin/pip install git+https://github.com/petermcd/docker-build.git;
 ./venv/bin/python build_dockerfile.py "$IMAGE_ID";
 
 docker stop "$IMAGE_ID";
+cd /tmp || exit ;
 docker build --tag petermcd/ssh_honeypot:$VERSION .;
 docker tag petermcd/ssh_honeypot:$VERSION petermcd/ssh_honeypot:latest;
